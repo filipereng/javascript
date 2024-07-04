db854a57976cd497dcd787ccb243bd1e182cc612
filipereng <172051783+filipereng@users.noreply.github.com>
2024-07-04 17:32:15 -0300

    while

--- aulas.js/5-repeticoes/index.js
+++ aulas.js/5-repeticoes/index.js
@@ -1,5 +1,17 @@
-var c = 1
-while (c > 3) {
-    console.log('Olá')
-    c++
+var i = 1
+    while (i < 10) {
+        console.log(`Passo ${i}`)
+        i++
+    }
+        console.log('FIM!')
+/*var i = 1
+        do {
+            console.log(`passo ${i}`)
+            i++
+        } while (i < 10);
+        console.log('FIM!!!')
+
+/*for (var i = 1; i < 10; i++) {
+    console.log(`Passos ${i}`)
 }
+console.log('FIM')*/
