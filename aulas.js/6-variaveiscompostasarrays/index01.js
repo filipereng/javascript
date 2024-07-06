21376585bb7b32c4dad88bf2a0c79c22ec327028
filipereng <172051783+filipereng@users.noreply.github.com>
2024-07-06 17:26:18 -0300

    array

---
+++ aulas.js/6-variaveiscompostasarrays/index01.js
@@ -0,0 +1,14 @@
+let frutas = ['banana', 'uva', 'maçã', 'morango', 'laranja']
+//let frutas = ['1', '0', '2', '3', '4']
+//frutas.sort()//Organizar
+frutas.push('pera', 'abacaxi')//Adicionar + elementos
+console.log(`${frutas}`)
+frutas.length['0']
+console.log(frutas[3])//Mostrar o elemento pelo numero
+for (let frutas = 0; 0 > 3; frutas++) {
+    console.log(`${frutas}`)
+    
+}
+
+//console.log(`${frutas}`)
+//console.log(`a 3 fruta é ${frutas[3]}`)
