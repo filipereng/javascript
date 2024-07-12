ed3d061fc43c0153bee5cc0129f4725fe8be8e7e
filipereng <172051783+filipereng@users.noreply.github.com>
2024-07-12 17:07:59 -0300

    js

---
+++ aulas.js/7-funcoes/funcao01.js
@@ -0,0 +1,23 @@
+var nome = 'Filipe'
+var idade = 30
+function mudarNomeIdade(n, i) {
+    nome = n
+    idade = i
+}
+console.log(nome, idade)
+
+mudarNomeIdade('Ana', 21)
+
+console.log(nome, idade)
+
+
+/*var nome = 'Filipe'
+
+function mudarNome(n, s) {
+    nome = n + s
+}
+console.log(nome)
+
+mudarNome('Julia', ' Ribeiro de Souza')
+
+console.log(nome)  */
---
+++ aulas.js/teste/teste.js
@@ -0,0 +1,7 @@
+var hora = 'horário'
+function horaIr() {
+    if (hora >= 6 && hora <= 10) 
+        return 'Vai!'
+    } else {
+        return 'NÃO VAI!'
+    }
