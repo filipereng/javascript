953024de20fe629ed66d4836a1bde2279c13712a
filipereng <172051783+filipereng@users.noreply.github.com>
2024-07-24 12:15:29 -0300

    js

--- aulas.js/teste/teste.js
+++ aulas.js/teste/teste.js
@@ -1,9 +1,3 @@
-var valor = 3
-
-if (valor <= 3) {
-    console.log('VALOR MENOR QUE 3')
-} else if 
-    console.log('VALOR MAIOR QUE 3')
-} else {
-    console.log('CABO')
+while (condition) {
+    while
 }
