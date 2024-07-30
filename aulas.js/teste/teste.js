d5009ef36d05f277b1aaacb3eb73abbc8630dbae
filipereng <172051783+filipereng@users.noreply.github.com>
2024-07-30 14:15:54 -0300

    funcao

--- aulas.js/teste/teste.js
+++ aulas.js/teste/teste.js
@@ -1,12 +1,3 @@
-
-function somaNum() {
-    let n1 = 1
-    let n2 = 2
-    let soma = n1 + n2
-    console.log(soma)
+for (let c = 0; c < 5; c++) {
+    console.log(` nota: ${c}`)
 }
-for (let i = 0; i < 5; i++) {
-    somaNum()
-}
-
-console.log(soma)
