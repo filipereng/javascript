14e556794fc1018590ec239038bb40d424b8059f
filipereng <172051783+filipereng@users.noreply.github.com>
2024-07-31 08:59:15 -0300

    function

--- aulas.js/teste/teste.js
+++ aulas.js/teste/teste.js
@@ -1,3 +1,8 @@
-for (let c = 0; c < 5; c++) {
-    console.log(` nota: ${c}`)
+function guardarCoisas(item1, item2, item3) {
+    const bau = `coisas guardadas no bau ${item1} ${item2} e ${item3}`
+    return bau
 }
+const baudoUsuario = guardarCoisas('gloves', 'boots', 'armor')
+console.log(baudoUsuario)
+
+
