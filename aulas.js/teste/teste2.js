350e94d3a99920a31541cbceb5345524732472b2
filipereng <172051783+filipereng@users.noreply.github.com>
2024-08-22 11:06:29 -0300

    js

--- aulas.js/teste/teste2.js
+++ aulas.js/teste/teste2.js
@@ -1,7 +1,6 @@
-var nome = 'Filipe'
-nome = nome.toUpperCase()
-console.log(nome);
-
-
-
-
+var nomes = ['ana', 'filipe', 'leo', 'zé']
+//var dados = {nome: 'filipe', idade: 30}
+for (var nome in nomes){
+    console.log(nomes[nome]);
+    
+}
