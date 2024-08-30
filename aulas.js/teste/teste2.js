e7bc772fcd6394b0cf5bb8b8bf381bdc1d2051f1
filipereng <172051783+filipereng@users.noreply.github.com>
2024-08-30 09:33:30 -0300

    js

--- aulas.js/teste/teste2.js
+++ aulas.js/teste/teste2.js
@@ -1,9 +1,7 @@
-const pessoas = [
-    {nome: 'ana', idade: 21},
-    {nome: 'filipe', idade: 30},
-    {nome: 'zé', idade: 40}
-]
+const pessoas = ['ana', 'filipe', 'zé', 'leo', 'new', 'now', 'ribeiro']
 
-let maiorIdade = pessoas.filter((i) => i.idade >= 25)
-return true
+const pessoas2 = ['ola', 'filipe', 'zé', 'leo', 'new', 'now', 'olé']
 
+const pessoas3 = ['pessoa333']
+
+console.log(pessoas.concat(pessoas2).concat(pessoas3));
